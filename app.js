c8856619015e6081cd38b52b9473860e590e1ff3
meothailand <sunnyvo@gmail.com>
2018-09-18 03:03:31 +0700

    commit

--- app.js
+++ app.js
@@ -1,20 +1,10 @@
 console.log('Starting app...');
+
 const fsystem = require('fs');
-const os = require('os');
-const note = require('./notes.js');
 const _ = require('loadsh');
 
-var res = note.add(3, -1);
-console.log(res);
+const note = require('./notes.js');
 
-console.log(_.isString(true));
-console.log(_.isString('Gary'));
 
-// var user = os.userInfo();
 
-// fsystem.appendFile('greeting.txt', `Hello ${user.username}! Your age is ${note.age}`, (err) => {
-//     if(err){
-//         console.log('Unable to write to file');
-//     }
-// });
 
