29a31a976ebcf9f271f9fd0e029b01461dade886
thamvn <thamvn@blockchainlabs.asia>
2018-09-18 19:09:08 +0700

    install new modules

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ const _ = require('loadsh');
 
 const note = require('./notes.js');
 
+var command = process.argv[2];
 
+switch(command){
+    case 'add':
+        break;
+    case 'list':
+        break;
+    case 'read':
+        break;
+    case 'remove':
+        break;
+    default:
+        console.log(`Command [${command}] not found`);
+}
 
 
